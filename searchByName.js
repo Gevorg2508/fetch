c5a67b27b661c990beb47db9fc340978bf7afc65
Gevorg Harutyunyan <gev.harutyunyan.96@gmail.com>
2022-02-07 20:06:49 +0400

    feat: here is my code main logic, but it is not working yet, now i solve this problem

--- searchByName.js
+++ searchByName.js
@@ -1,10 +1,54 @@
-fetch("https://restcountries.com/v3.1/name/peru")
-.then(res => res.json())
-.then(data => console.log(data))
-.then(elem => {
-    for (let i = 0; i<elem.length; i++){
-        if(elem[i] === name){
-            console.log(elem[i]);
+const apiUrl = "https://restcountries.com/v3.1";
+async function fetchPosts() {
+    try {
+        const response = await fetch(`${apiUrl}/name`);
+
+        if (!response.ok) {
+            throw new Error(`failed to fetch ${response.status}`)
         }
+
+        return await response.json()
+    } catch (error) {
+        console.log(error);
     }
-})
+}
+
+function listPosts(postContainerElementId) {
+    const postContainerElement = document.getElementById(postContainerElementId);
+    if (!postContainerElement) {
+        return;
+    }
+
+    fetchPosts().then(posts => {
+        if(!posts){
+            postContainerElement.innerHTML = "No posts fetched.";
+            return;
+        }
+
+        for(post of posts){
+            postContainerElement.appendChild(postElement(post));
+        }
+     })
+        .catch(e => {
+            console.log(e);
+        })
+}
+
+function postElement(post){
+    const anchorElement = document.createElement('a');
+    anchorElement.setAttribute('href', `${apiUrl}/name/${post.common}`)
+    anchorElement.setAttribute('target', '_blank');
+    anchorElement.innerText = capitalizeFirstLetter(post.title)
+
+    postTitleElement = document.createElement('h3');
+    postTitleElement.appendChild(anchorElement);
+
+
+
+    return postTitleElement;
+}
+function capitalizeFirstLetter(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
